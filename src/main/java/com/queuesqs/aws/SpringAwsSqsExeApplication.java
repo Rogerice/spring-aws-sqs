ba15b97e1f3a1f6c4f362309396eb624d88989f3
rogerice <rogerrodriguesti@gmail.com>
2023-02-18 11:12:23 -0300

    Classe de configuração

---
+++ src/main/java/com/queuesqs/aws/SpringAwsSqsExeApplication.java
@@ -0,0 +1,13 @@
+package com.queuesqs.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsSqsExeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsSqsExeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/queuesqs/aws/SpringAwsSqsExeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queuesqs.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsSqsExeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
